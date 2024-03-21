e64144c55a95a513ffef06e4dde98ee6583f96f0
Bektash <tok1baev67@gmail.com>
2024-03-21 21:34:57 +0600

    install commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+import {Routes, Route } from "react-router";
+import Posts from "./components/Posts/Posts";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css'
+import PostForm from "./components/PostForm/PostForm";
 function App() {
+
   return (
     <div className="App">
-      Hello my first
+      <Routes>
+        <Route path="/" element={<Posts/>}/>
+        <Route path="/posts" element={<PostForm/>}/>
+
+      </Routes>
     </div>
   );
 }
